fac7b4c3fb729513b53ef47746591a821c2ee13c
Vinicius Avena <vini.avena@live.com>
2022-02-02 22:14:06 -0300

    users model

---
+++ app/users/model.py
@@ -0,0 +1,23 @@
+from app.models import BaseModel
+from ..extensions import db
+import bcrypt
+
+
+class User(BaseModel):
+    __tablename__ = 'users'
+
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=False, unique = True)
+
+    password_hash = db.Column(db.LargeBinary(128), nullable = False)
+    
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode(), self.password_hash)
+    
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
